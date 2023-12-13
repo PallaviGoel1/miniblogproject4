c802f0361a23e71d7deff9efb4a136cf7d3e4d61
Pallavi Goel <pallavigoel2610@gmail.com>
2023-12-13 13:48:34 +0000

    add favicon icon in base file

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -44,13 +44,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
-    'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'django.contrib.staticfiles',
+    'allauth.socialaccount',
     'django_summernote',
+    'allauth.account',
     'miniblog',
 
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -60,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'blogproj.urls'
